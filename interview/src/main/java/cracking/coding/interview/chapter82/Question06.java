d01b5e7ecda2fab2e4789fa7ec9e96f49b8dbbd3
macrosf@home <macros_f@163.com>
2016-12-22 22:15:33 +0800

    question 06

--- interview/src/main/java/cracking/coding/interview/chapter82/Question06.java
+++ interview/src/main/java/cracking/coding/interview/chapter82/Question06.java
@@ -18,7 +18,7 @@ public class Question06 {
 		if (fast == null || fast.next == null) return null;
 		
 		slow = head;
-		while (slow.next != fast){
+		while (slow != fast){
 			slow = slow.next;
 			fast = fast.next;
 		}
@@ -26,6 +26,16 @@ public class Question06 {
 	}
 	
 	public static void main(String[] args){
+		LinkedListNode top = new LinkedListNode(1)
+				.append(2)
+				.append(3)
+				.append(4);
+		LinkedListNode n = new LinkedListNode(5);
+		top.append(n);
+		n.next = top.next.next; //n.next -> 3
 		
+		LinkedListNode begin = findBeginning(top);
+		if (begin==null) System.out.println("no connection");
+		else System.out.println(begin.data);
 	}
 }
